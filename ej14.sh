f3f3b5cb586d6edc6c5193c7b4113d8616ed5deb
Juandonado25 <juandonado25@gmail.com>
2024-09-27 11:36:14 -0300

    act

--- ej14.sh
+++ ej14.sh
@@ -2,27 +2,35 @@
 #
 #Renombra los archivos en un directorio pasado como parametro, agregando al principio o al final
 #de su nombre una cadena de texto tambien pasada por parametro.
+#	-a para insertar al final del nombre.
+#	-b para insertar al principio del nombre.
 
+#Valida los parametros
 if [ $# -ne 3 ]; then
 	echo "Se deben pasar 3 parametros y se pasaron $#"
 	exit 1
 fi
 
+#Verifica que los parametros sean correctos
 if [ "$2" != "-a" ] && [ "$2" != "-b" ]; then
 	echo "Parametro erroneo."
+	exit 1
 fi
+
+#Asigno los parametros a variables para que sea mas legible
 directorio=$1
 extra=$3
 
 if [ -e $directorio ] && [ -d $directorio ]; then
-	IFS=$'\n' archivos=($(ls $1))
+	#Se hace lo del IFS para poder leer el salto de linea
+	IFS=$'\n' archivos=($(ls "$directorio"))
 	unset IFS
-	for archivo in $archivos; do
-		if [ -f $archivo ]; then
+	for archivo in ${archivos[@]}; do
+		if [ -f "$directorio/$archivo" ]; then
 			if [ "$2" = "-a" ]; then
-				mv "${directorio}${archivo}" "${directorio}${archivo}${extra}"
+				mv "$directorio/$archivo" "$directorio/${archivo}${extra}"
 			else
-				$(mv "${$1}${$archivo}" "${$1}${$3}${$archivo}")
+				mv "$directorio/$archivo" "${directorio}/${extra}${archivo}"
 			fi
 		fi
 	done
