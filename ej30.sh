02a8f83b221874e0837f9bcbcc14c2076f619804
Juandonado25 <juandonado25@gmail.com>
2024-10-02 11:43:47 -0300

    practica 3 terminada

--- ej30.sh
+++ ej30.sh
@@ -5,6 +5,17 @@
 #que mueve e indica cuantos ha movido o si no ha movido ninguno.
 #Si el directorio "bin" no existe lo crea.
 
-userhome="$(cat /etc/passwd | grep $(whoami) | cut -d ":" -f 6)"
+userhome="$(cat /etc/passwd | cut -d ":" -f 6 | grep $(whoami))"
 
-echo "$userhome"
+if ! [ -e "${userhome}/bin/" ]; then
+	sudo mkdir "${userhome}/bin"
+fi
+
+archivos=($(ls))
+
+for arch in ${archivos[*]}; do
+	if ! [ -d $arch ] && [ -x $arch ]; then
+		sudo mv $arch "${userhome}/bin/"
+		echo "Se movio $arch"
+	fi
+done
